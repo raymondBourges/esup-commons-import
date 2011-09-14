20eccc23dac6f15a95d6958198224fd6234465f2
cbissler <cbissler@ac49bf0c-5512-0410-ad41-c1b246f41ce7>
2011-09-14 09:47:48 +0000

    Rectif exo 13

--- esup-formation-utils/src/main/java/org/esupportail/formation/utils/TaskDateComparator.java
+++ esup-formation-utils/src/main/java/org/esupportail/formation/utils/TaskDateComparator.java
@@ -7,10 +7,22 @@ import org.esupportail.formation.domain.beans.Task;
 public class TaskDateComparator implements Comparator<Task> {
 	public int compare(Task t1, Task t2) 
 	  {
-	    int result =  t1.getDate().compareTo(t2.getDate());
-	    //si la date est identique on trie ensuite par le titre
-	    if(result==0)
-	      result = t1.getTitle().compareTo(t2.getTitle());
-	  	return result;
+		if (t1.getDate()==null)
+			return -1;
+		else if (t2.getDate()==null)
+			return 1;
+		else{
+		    int result =  t1.getDate().compareTo(t2.getDate());
+		    //si la date est identique on trie ensuite par le titre
+		    if(result==0){
+		    	if (t1.getTitle()==null)
+		    		result =  -1;
+				else if (t2.getTitle()==null)
+					result =  1;
+				else
+					result = t1.getTitle().compareTo(t2.getTitle());
+		    }
+		    return result;
+		}
 	  }
 }
--- esup-formation-utils/src/main/java/org/esupportail/formation/utils/TaskTitleComparator.java
+++ esup-formation-utils/src/main/java/org/esupportail/formation/utils/TaskTitleComparator.java
@@ -7,10 +7,22 @@ import org.esupportail.formation.domain.beans.Task;
 public class TaskTitleComparator implements Comparator<Task> {
 	public int compare(Task t1, Task t2) 
 	  {
-	    int result = t1.getTitle().compareTo(t2.getTitle());
-	    //si le titre est identique on trie ensuite par la date
-	    if(result==0)
-	      result = t1.getDate().compareTo(t2.getDate());
-	  	return result;
+		if (t1.getTitle()==null)
+			return -1;
+		else if (t2.getTitle()==null)
+			return 1;
+		else{
+		    int result =  t1.getTitle().compareTo(t2.getTitle());
+		    //si le titre est identique on trie ensuite par la date
+		    if(result==0){
+		    	if (t1.getDate()==null)
+		    		result =  -1;
+				else if (t2.getDate()==null)
+					result =  1;
+				else
+					result = t1.getDate().compareTo(t2.getDate());
+		    }
+		    return result;
+		}
 	  }
 }
